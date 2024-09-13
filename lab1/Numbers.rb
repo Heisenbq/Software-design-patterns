f4f88435d71f7cc10b41b5b5b6511f7262dba7fb
Gadjiev Akhmed <a.gadjiev1@mail.ru>
2024-09-13 23:14:00 +0300

    Third method

--- lab1/Numbers.rb
+++ lab1/Numbers.rb
@@ -26,5 +26,57 @@ def min_odd_digit_of_number(number)
 	return res_digit
 end
 
+# 3 подпункт
+
+# def max(num1,num2)
+# 	if num1>num2 then return num1 end
+# 	return num2
+# end
+
+# def mutually_primes(number1,number2)
+# 	bigger = max(number1,number2)
+# 	div=2
+# 	while div<bigger
+# 		if number1%div==0 && number2%div==0 then return false end
+# 		div+=1
+# 	end
+# 	return true
+# end
+def euclidean_gcd(a, b)
+  a_copy = a
+  b_copy = b
+  while b_copy != 0
+    a_copy, b_copy = b_copy, a_copy % b_copy
+  end
+  a_copy
+end
+
+def mutually_primes(number1,number2)
+	divider=euclidean_gcd(number1,number2)
+	if divider == 1 then return true end
+	return false
+end
+
+def sum_of_dividers_mutually_primes(number)
+	sum_of_div=0
+	number_copy=number
+	sum_digits=0
+	multi_digits=1
+
+	while number_copy > 0
+		digit = number_copy%10
+		number_copy /= 10
+		sum_digits+=digit
+		multi_digits*=digit
+	end
+	div=2
+	puts number
+	while div*div <= number
+		if number%div==0 && mutually_primes(div,sum_digits)&& !mutually_primes(div,multi_digits)  then sum_of_div+=2 end
+
+		div+=1
+	end
+	return sum_of_div
+end
 
 
