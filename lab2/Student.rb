60edc07454f15be7c5c03d62d75664d4a4726f88
Gadjiev Akhmed <a.gadjiev1@mail.ru>
2024-09-27 19:44:40 +0300

    add validation on phone number

--- lab2/Student.rb
+++ lab2/Student.rb
@@ -1,11 +1,14 @@
 class Student
-  attr_accessor :phone, :telegram, :email, :github, :id, :surname, :first_name, :last_name
-  
+  attr_reader :phone, :telegram, :email, :github, :id, :surname, :first_name, :last_name
+  attr_writer :telegram, :email, :github, :id, :surname, :first_name, :last_name
     
   def initialize(args)
     @id = args[:id]
     if (!args[:surname] || !args[:first_name] || !args[:last_name]) then
       raise "Exception some of attributes not given"
+    end 
+    if !valid_phone?(args[:phone]) then 
+      raise "Not valid phone number"
     end 
     @surname = args[:surname]
     @first_name = args[:first_name]
@@ -16,7 +19,19 @@ class Student
     @github = args[:github]
   end
 
+  def phone=(phone)
+    if !valid_phone?(phone) then 
+      raise "Not valid phone number"
+    end 
+    @phone = phone
+  end
+
+
   def to_s
     " ID: #{@id}\n ФИО: #{@surname} #{@first_name} #{@last_name}\n Телефон: #{@phone}\n Телеграм: #{@telegram}\n Почта: #{@email}\n Git: #{@github}"
+  end
+
+  def valid_phone?(phone)
+    !!(phone =~ /^(?:\+7|8)[\s-]?(?:\(?\d{3}\)?[\s-]?)\d{3}[\s-]?\d{2}[\s-]?\d{2}$/)
   end
 end
