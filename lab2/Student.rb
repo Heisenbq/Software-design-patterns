b254f7ab65fe8ba5a1d7665036b1f39fe00da103
Gadjiev Akhmed <a.gadjiev1@mail.ru>
2024-09-27 18:31:51 +0300

    fix task 1, write getters and setters without attr_acccessor

--- lab2/Student.rb
+++ lab2/Student.rb
@@ -2,7 +2,7 @@ class Student
   attr_accessor :phone, :telegram, :email, :github, :id, :surname, :first_name, :last_name
 
     
-  def initialize(id, surname, first_name, last_name, phone: nil, telegram: nil, email: nil, github: nil)
+  def initialize(surname, first_name, last_name, id = nil, phone = nil, telegram = nil, email = nil, github = nil)
     @id = id
     @surname = surname
     @first_name = first_name
@@ -10,6 +10,86 @@ class Student
     @phone = phone
     @telegram = telegram
     @email = email
+    @github = github
+  end
+
+  # Геттер для фамилии
+  def surname
+    @surname
+  end
+
+  # Сеттер для фамилии
+  def surname=(surname)
+    @surname = surname
+  end
+
+  # Геттер для имени
+  def first_name
+    @first_name
+  end
+
+  # Сеттер для имени
+  def first_name=(first_name)
+    @first_name = first_name
+  end
+
+  # Геттер для отчества
+  def last_name
+    @last_name
+  end
+
+  # Сеттер для отчества
+  def last_name=(last_name)
+    @last_name = last_name
+  end
+
+  # Геттер для ID
+  def id
+    @id
+  end
+
+  # Сеттер для ID
+  def id=(id)
+    @id = id
+  end
+
+  # Геттер для телефона
+  def phone
+    @phone
+  end
+
+  # Сеттер для телефона
+  def phone=(phone)
+    @phone = phone
+  end
+
+  # Геттер для Telegram
+  def telegram
+    @telegram
+  end
+
+  # Сеттер для Telegram
+  def telegram=(telegram)
+    @telegram = telegram
+  end
+
+  # Геттер для почты
+  def email
+    @email
+  end
+
+  # Сеттер для почты
+  def email=(email)
+    @email = email
+  end
+
+  # Геттер для GitHub
+  def github
+    @github
+  end
+
+  # Сеттер для GitHub
+  def github=(github)
     @github = github
   end
 end
