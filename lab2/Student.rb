7de72baa3dfbf71b4608d20578b293e19036b61c
Gadjiev Akhmed <a.gadjiev1@mail.ru>
2024-09-27 18:37:48 +0300

    avoid duplication of code in getters setters

--- lab2/Student.rb
+++ lab2/Student.rb
@@ -1,5 +1,6 @@
 class Student
-  attr_accessor :phone, :telegram, :email, :github, :id, :surname, :first_name, :last_name
+  attr_reader :phone, :telegram, :email, :github, :id, :surname, :first_name, :last_name
+  attr_writer :phone, :telegram, :email, :github, :id, :surname, :first_name, :last_name
 
     
   def initialize(surname, first_name, last_name, id = nil, phone = nil, telegram = nil, email = nil, github = nil)
@@ -10,86 +11,6 @@ class Student
     @phone = phone
     @telegram = telegram
     @email = email
-    @github = github
-  end
-
-  # Геттер для фамилии
-  def surname
-    @surname
-  end
-
-  # Сеттер для фамилии
-  def surname=(surname)
-    @surname = surname
-  end
-
-  # Геттер для имени
-  def first_name
-    @first_name
-  end
-
-  # Сеттер для имени
-  def first_name=(first_name)
-    @first_name = first_name
-  end
-
-  # Геттер для отчества
-  def last_name
-    @last_name
-  end
-
-  # Сеттер для отчества
-  def last_name=(last_name)
-    @last_name = last_name
-  end
-
-  # Геттер для ID
-  def id
-    @id
-  end
-
-  # Сеттер для ID
-  def id=(id)
-    @id = id
-  end
-
-  # Геттер для телефона
-  def phone
-    @phone
-  end
-
-  # Сеттер для телефона
-  def phone=(phone)
-    @phone = phone
-  end
-
-  # Геттер для Telegram
-  def telegram
-    @telegram
-  end
-
-  # Сеттер для Telegram
-  def telegram=(telegram)
-    @telegram = telegram
-  end
-
-  # Геттер для почты
-  def email
-    @email
-  end
-
-  # Сеттер для почты
-  def email=(email)
-    @email = email
-  end
-
-  # Геттер для GitHub
-  def github
-    @github
-  end
-
-  # Сеттер для GitHub
-  def github=(github)
     @github = github
   end
 
