f3bf9006bcd3e91d3145cfc00d98889f3d0a0897
Gadjiev Akhmed <a.gadjiev1@mail.ru>
2024-10-04 22:28:01 +0300

    perform validation for all fields

--- lab2/Main.rb
+++ lab2/Main.rb
@@ -1,7 +1,7 @@
 require './Student.rb'
 
-student1 = Student.new(first_name: "Akhmed",last_name: "Ruslanovich",surname: "Gadjiev", id: 1, phone: "8 960 480 74 23", telegram: "@yoyo", email: "asd@mail.ru", github: "https://github.com/Heisenbq")
-student2 = Student.new(surname: "Gadjiev",first_name: "Akhmed",last_name: "Ruslanovich", email: "asd@mail.ru",phone: "8-960-480-74-23",  id: 1, telegram: "@yoyo",  github: "https://github.com/Heisenbq")
-student1.phone = "8 800 646 66 50"
+student1 = Student.new(first_name: "Akhmed",last_name: "Ruslanovich",surname: "Gadjiev", id: 1, phone: "8 960 480 74 23", telegram: "@valid_username", email: "asd@mail.ru", github: "https://github.com/Heisenbq")
+student2 = Student.new(surname: "Gadjiev",first_name: "Akhmed",last_name: "Ruslanovich", email: "asd@mail.ru",phone: "8-960-480-74-23",  id: 2, telegram: "@valid_username",  github: "https://github.com/Heisenbq")
+# student1.phone = "8 800 646 66 50"
 puts student1 , "\n"
 puts student2
--- lab2/Student.rb
+++ lab2/Student.rb
@@ -1,37 +1,90 @@
 class Student
   attr_reader :phone, :telegram, :email, :github, :id, :surname, :first_name, :last_name
-  attr_writer :telegram, :email, :github, :id, :surname, :first_name, :last_name
+  attr_writer  :id
     
   def initialize(args)
     @id = args[:id]
-    if (!args[:surname] || !args[:first_name] || !args[:last_name]) then
-      raise "Exception some of attributes not given"
-    end 
-    if !valid_phone?(args[:phone]) then 
-      raise "Not valid phone number"
-    end 
-    @surname = args[:surname]
-    @first_name = args[:first_name]
-    @last_name = args[:last_name]
-    @phone = args[:phone]
-    @telegram = args[:telegram]
-    @email = args[:email]
-    @github = args[:github]
+    if (!args[:surname] || !args[:first_name] || !args[:last_name])
+      raise "Exception: some of the required attributes are not given"
+    end
+
+    self.surname = args[:surname]
+    self.first_name = args[:first_name]
+    self.last_name = args[:last_name]
+    self.phone = args[:phone]
+    self.telegram = args[:telegram]
+    self.email = args[:email]
+    self.github = args[:github]
+  end
+
+  def surname=(surname)
+    if !valid_name?(surname)
+      raise ArgumentError, "Invalid surname format"
+    end
+    @surname = surname
+  end
+
+  def first_name=(first_name)
+    if !valid_name?(first_name)
+      raise ArgumentError, "Invalid first name format"
+    end
+    @first_name = first_name
+  end
+
+  def last_name=(last_name)
+    if !valid_name?(last_name)
+      raise ArgumentError, "Invalid last name format"
+    end
+    @last_name = last_name
   end
 
   def phone=(phone)
-    if !valid_phone?(phone) then 
-      raise "Not valid phone number"
+    if !valid_phone?(phone)
+      raise  ArgumentError, "Not valid phone number"
     end 
     @phone = phone
   end
 
+  def email=(email)
+    if !valid_email?(email)
+      raise ArgumentError, "Invalid email format"
+    end
+    @email = email
+  end
 
+  def telegram=(telegram)
+    if !valid_telegram?(telegram)
+      raise ArgumentError, "Invalid Telegram username"
+    end
+    @telegram = telegram
+  end
+
+  def github=(github)
+    if !valid_github?(github)
+      raise ArgumentError, "Invalid GitHub profile URL"
+    end
+    @github = github
+  end
+  
   def to_s
     " ID: #{@id}\n ФИО: #{@surname} #{@first_name} #{@last_name}\n Телефон: #{@phone}\n Телеграм: #{@telegram}\n Почта: #{@email}\n Git: #{@github}"
   end
 
+  private 
+
   def valid_phone?(phone)
     !!(phone =~ /^(?:\+7|8)[\s-]?(?:\(?\d{3}\)?[\s-]?)\d{3}[\s-]?\d{2}[\s-]?\d{2}$/)
+  end
+  def valid_name?(name)
+    !!(name =~ /^[a-zA-Zа-яА-ЯёЁ]+$/)
+  end
+  def valid_email?(email)
+    !!(email=~/^[\w+\-.]+@[a-z\d\-.]+\.[a-z]+$/i)
+  end
+  def valid_telegram?(telegram)
+    !!(telegram=~/^@?[a-zA-Z0-9_]{5,32}$/)
+  end
+  def valid_github?(github)
+    !!(github=~/^https:\/\/github\.com\/[a-zA-Z0-9\-]{1,39}$/)
   end
 end
