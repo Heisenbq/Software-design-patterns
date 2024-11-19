f36857327f28bdcd7c805f4996774663c7948c8a
Gadjiev Akhmed <a.gadjiev1@mail.ru>
2024-11-19 15:27:05 +0300

    4.2 create methods for DataTable

--- ClassModels/Data_table.rb
+++ ClassModels/Data_table.rb
@@ -6,6 +6,21 @@ class DataTable
     @data = data
   end
 
+  def get_element(num_of_row,num_of_column)
+    @data[num_of_row][num_of_column]
+  end
+  def count_of_rows
+    @data.count
+  end
+  def count_of_columns
+    @data[0].count
+  end
   private
   attr_reader :data
 end
+
+table = DataTable.new([[1,2,3,4],
+                      [4,5,6,4],
+                      [7,8,9,4]])
+
+puts table.get_element(0,1)
