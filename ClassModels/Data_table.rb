868733d690a8e16c5572899d86b885dfaeeb6764
Gadjiev Akhmed <a.gadjiev1@mail.ru>
2024-11-19 15:19:18 +0300

    4.1 refactor

--- ClassModels/Data_table.rb
+++ ClassModels/Data_table.rb
@@ -1,16 +1,11 @@
 class DataTable
   def initialize(data)
     unless data.is_a?(Array) && data.all? {|e| e.is_a?(Array)}
-      raise ArgumentError, "Data is not an Array"
+      raise ArgumentError, "Data is not an 2d Array"
     end
     @data = data
   end
 
+  private
+  attr_reader :data
 end
-
-table = DataTable.new([
-                        [1,2,3],
-                        [4,5,6],
-                        [7,8,9],
-                      ])
-puts table.to_s
