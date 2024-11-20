88ac4f99ce8eda4fddfffc5850184da81a95b7fd
Gadjiev Akhmed <a.gadjiev1@mail.ru>
2024-11-20 14:06:49 +0300

    4.1 refactor folders

---
+++ StudentsLab/DataList/DataListStudentShort.rb
@@ -0,0 +1,32 @@
+require File.expand_path('D:/3курс/RubyProjects/StudentsLab/DataList/DataList.rb')
+require File.expand_path('D:/3курс/RubyProjects/StudentsLab/DataList/DataTable.rb')
+require File.expand_path('D:/3курс/RubyProjects/StudentsLab/Models/Student_short.rb')
+require File.expand_path('D:/3курс/RubyProjects/StudentsLab/Models/Student.rb')
+class DataListStudentShort < DataList
+
+  private
+  def get_names_of_attributes_of_object
+    ['№', 'contact', 'github', 'surname_initials']
+  end
+
+  
+  def get_row_of_values_of_object(index,student_short)
+    [index, student_short.contact, student_short.surname_initials,student_short.github]
+  end
+
+  def check_object_type(data)
+    data.all? {|elem| elem.is_a?(Student_short)} 
+  end
+end
+student1 = Student.new(first_name: "Akhmed",last_name: "Ruslanovich",surname: "Gadjiev", github: "https://github.com/Heisenbq")
+student2 = Student.new(surname: "Gadjiev",first_name: "Akhmed",last_name: "Ruslanovich", email: "asd@mail.ru",phone: "8-960-480-74-23",  id: 2, telegram: "@valid_username",  github: "https://github.com/Heisenbq")
+student3 = Student.new(first_name: "Akhmed",last_name: "Ruslanovich",surname: "Gadjiev", github: "https://github.com/Heisenbq")
+student4 = Student.new(first_name: "Akhmed",last_name: "Ruslanovich",surname: "Gadjiev", github: "https://github.com/Heisenbq")
+st1 = Student_short.create_by_student(student1)
+st2 = Student_short.create_by_student(student2)
+st3 = 1
+list = DataListStudentShort.new([st1,st2])
+list.select(0)
+list.select(1)
+puts list.get_data
+
