8a35a5e03ae01c0e175e5858ce380cdbbd3a839a
Gadjiev Akhmed <a.gadjiev1@mail.ru>
2024-11-21 12:48:19 +0300

    4.2.2. e-f  sort ans addition of student

--- StudentsLab/Serializers/StudentListJSON.rb
+++ StudentsLab/Serializers/StudentListJSON.rb
@@ -5,10 +5,13 @@ require File.expand_path('D:/3курс/RubyProjects/StudentsLab/Models/DataList/
 require 'json'
 
 class StudentListJSON
+  def initialize(path)
+    @path = path
+    @students
+  end
 
-
-  def self.read_from_file(path)
-    json_data = File.read(path)
+  def read_from_file
+    json_data = File.read(@path)
     students_data = JSON.parse(json_data)
     students = students_data.map do |student_data|
       Student.new(
@@ -23,16 +26,16 @@ class StudentListJSON
         github: student_data['github']
       )
     end
-    students
+    @students = students
   end
   
-  def self.write_to_file(path, students)
+  def write_to_file(students)
     json_data = JSON.pretty_generate(students.map(&:to_hash))
-    File.write(path,json_data)
+    File.write(@path,json_data)
   end
 
-  def self.get_student_by_id(path,id)
-    json_data = File.read(path)
+  def get_student_by_id(id)
+    json_data = File.read(@path)
     students_data = JSON.parse(json_data)
     student = students_data.find do |student|
       student['id'] == id
@@ -51,8 +54,8 @@ class StudentListJSON
       )
   end
 
-  def self.get_student_short_list(path,amount_of_elems_on_page,page)
-    json_data = File.read(path)
+  def get_student_short_list(amount_of_elems_on_page,page)
+    json_data = File.read(@path)
     students_data = JSON.parse(json_data)
     student_slice = students_data.each_slice(amount_of_elems_on_page).to_a
     student_short_array = []
@@ -72,8 +75,22 @@ class StudentListJSON
     end
     DataListStudentShort.new(student_short_array)
   end
+
+  def sort_by_surname_initials
+    @students.sort_by {|element| element.surname_initials}
+  end
+
+  def add_student_in_list(student)
+    if !student.is_a?(Student) raise ArgumentError, "Expect the instance of Student"
+    max_id = @students.max_by {|el| el.id}
+    student.id = max_id + 1
+    @students.append(student)
+  end
 end
 
-students = StudentListJSON.read_from_file('D:/3курс/RubyProjects/StudentsLab/files_for_tests/StudentList.json')
-StudentListJSON.write_to_file('D:/3курс/RubyProjects/StudentsLab/files_for_tests/StudentList1.json',students)
-puts StudentListJSON.get_student_short_list('D:/3курс/RubyProjects/StudentsLab/files_for_tests/StudentList1.json',5,1).get_data
+a = StudentListJSON.new('D:/3курс/RubyProjects/StudentsLab/files_for_tests/StudentList.json')
+a.read_from_file
+puts a.sort_by_surname_initials
+# students = StudentListJSON.read_from_file('D:/3курс/RubyProjects/StudentsLab/files_for_tests/StudentList.json')
+# StudentListJSON.write_to_file('D:/3курс/RubyProjects/StudentsLab/files_for_tests/StudentList1.json',students)
+# puts StudentListJSON.get_student_short_list('D:/3курс/RubyProjects/StudentsLab/files_for_tests/StudentList1.json',5,1).get_data
