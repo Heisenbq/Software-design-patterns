05e432dbf574cff309846d2914fc2fb504d2ddab
Gadjiev Akhmed <a.gadjiev1@mail.ru>
2024-12-13 21:51:22 +0300

    5.0 update layout settings

--- StudentsLab/views/StudentListView.rb
+++ StudentsLab/views/StudentListView.rb
@@ -10,167 +10,151 @@ include Fox
 class StudentListView < FXMainWindow
   def initialize(app)
     super(app, "Student Information Table", width: 800, height: 400)
-  
+    setup_layuot()
+    setup_filter_frame()
+    setup_table_frame()
+    setup_crud_frame()
+  end
+
+  def setup_layuot()
     main_frame = FXVerticalFrame.new(self, opts: LAYOUT_FILL)
     # Контейнер для размещения фреймов горизонтально
     top_frame = FXHorizontalFrame.new(main_frame, opts: LAYOUT_FILL)
     
     # Левый фрейм (контейнер для дополнительных элементов, занимает 20% ширины)
-    frame_left = FXVerticalFrame.new(top_frame, opts: LAYOUT_SIDE_LEFT)
+    @frame_left = FXVerticalFrame.new(top_frame, opts: LAYOUT_SIDE_LEFT)
     
     # Правый фрейм (занимает оставшиеся 80% ширины)
-    frame_right = FXVerticalFrame.new(top_frame, opts: LAYOUT_FILL | LAYOUT_SIDE_RIGHT)
-    FXLabel.new(frame_left, "Фильтрация")
-  
-    # Создаем таблицу
-    table = FXTable.new(frame_right, opts: LAYOUT_FILL)
-    
-    buttom_frame = FXHorizontalFrame.new(frame_right, opts: LAYOUT_SIDE_BOTTOM)
+    @frame_right = FXVerticalFrame.new(top_frame, opts: LAYOUT_FILL | LAYOUT_SIDE_RIGHT)
+
+    @buttom_frame = FXHorizontalFrame.new(@frame_right, opts: LAYOUT_SIDE_BOTTOM)
+  end
+
+  def setup_filter_frame()
+    FXLabel.new(@frame_left, "Фильтрация")
 
     # Добавление метки
-    FXLabel.new(frame_left, "Введите Фамилию и ИО:")
+    FXLabel.new(@frame_left, "Введите Фамилию и ИО:")
   
     # Поле для ввода текста
-    @text_field = FXTextField.new(frame_left, 20)
+    text_field = FXTextField.new(@frame_left, 20)
 
-    FXLabel.new(frame_left, "Наличие гита:")
+    FXLabel.new(@frame_left, "Наличие гита:")
 
     # Создание ComboBox с тремя элементами
-    @combo_box_git = FXComboBox.new(frame_left, 3, opts: COMBOBOX_STATIC | FRAME_SUNKEN | FRAME_THICK)
-    @combo_box_git.numVisible = 3  # Количество видимых элементов в раскрытом списке
+    combo_box_git = FXComboBox.new(@frame_left, 3, opts: COMBOBOX_STATIC | FRAME_SUNKEN | FRAME_THICK)
+    combo_box_git.numVisible = 3  # Количество видимых элементов в раскрытом списке
 
     # Добавление вариантов в ComboBox
-    @combo_box_git.appendItem("не важно")
-    @combo_box_git.appendItem("нет")
-    @combo_box_git.appendItem("есть")
+    combo_box_git.appendItem("не важно")
+    combo_box_git.appendItem("нет")
+    combo_box_git.appendItem("есть")
 
-    @combo_box_git.currentItem = 0
+    combo_box_git.currentItem = 0
 
-    @git_field = FXTextField.new(frame_left, 20)
-    @git_field.text = "Поиск по гиту"
+    git_field = FXTextField.new(@frame_left, 20)
+    git_field.text = "Поиск по гиту"
 
-    @git_field.enabled = false
+    git_field.enabled = false
     
-    @combo_box_git.connect(SEL_COMMAND) do
-      case @combo_box_git.currentItem
+    combo_box_git.connect(SEL_COMMAND) do
+      case combo_box_git.currentItem
       when 0
-        @git_field.enabled = false 
+        git_field.enabled = false 
       when 1
-        @git_field.enabled = false        # Поле полностью отключено
+        git_field.enabled = false        # Поле полностью отключено
       when 2
-        @git_field.enabled = true         # Поле доступно для ввода
-        @git_field.editable = true
+        git_field.enabled = true         # Поле доступно для ввода
+        git_field.editable = true
       end
     end
 
-    FXLabel.new(frame_left, "Наличие почты:")
-    @combo_box_email = FXComboBox.new(frame_left, 3, opts: COMBOBOX_STATIC | FRAME_SUNKEN | FRAME_THICK)
-    @combo_box_email.numVisible = 3  # Количество видимых элементов в раскрытом списке
+    FXLabel.new(@frame_left, "Наличие почты:")
+    combo_box_email = FXComboBox.new(@frame_left, 3, opts: COMBOBOX_STATIC | FRAME_SUNKEN | FRAME_THICK)
+    combo_box_email.numVisible = 3  # Количество видимых элементов в раскрытом списке
 
     # Добавление вариантов в ComboBox
-    @combo_box_email.appendItem("не важно")
-    @combo_box_email.appendItem("нет")
-    @combo_box_email.appendItem("есть")
+    combo_box_email.appendItem("не важно")
+    combo_box_email.appendItem("нет")
+    combo_box_email.appendItem("есть")
 
-    @combo_box_email.currentItem = 0
+    combo_box_email.currentItem = 0
 
-    @email_field = FXTextField.new(frame_left, 20)
-    @email_field.text = "Поиск по почте"
+    email_field = FXTextField.new(@frame_left, 20)
+    email_field.text = "Поиск по почте"
 
-    @email_field.enabled = false
+    email_field.enabled = false
     
-    @combo_box_email.connect(SEL_COMMAND) do
-      case @combo_box_email.currentItem
+    combo_box_email.connect(SEL_COMMAND) do
+      case combo_box_email.currentItem
       when 0
-        @email_field.enabled = false 
+        email_field.enabled = false 
       when 1
-        @email_field.enabled = false        # Поле полностью отключено
+        email_field.enabled = false        # Поле полностью отключено
       when 2
-        @email_field.enabled = true         # Поле доступно для ввода
-        @email_field.editable = true
+        email_field.enabled = true         # Поле доступно для ввода
+        email_field.editable = true
       end
     end
 
-    FXLabel.new(frame_left, "Наличие телефона:")
-    @combo_box_phone = FXComboBox.new(frame_left, 3, opts: COMBOBOX_STATIC | FRAME_SUNKEN | FRAME_THICK)
-    @combo_box_phone.numVisible = 3  # Количество видимых элементов в раскрытом списке
+    FXLabel.new(@frame_left, "Наличие телефона:")
+    combo_box_phone = FXComboBox.new(@frame_left, 3, opts: COMBOBOX_STATIC | FRAME_SUNKEN | FRAME_THICK)
+    combo_box_phone.numVisible = 3  # Количество видимых элементов в раскрытом списке
 
     # Добавление вариантов в ComboBox
-    @combo_box_phone.appendItem("не важно")
-    @combo_box_phone.appendItem("нет")
-    @combo_box_phone.appendItem("есть")
+    combo_box_phone.appendItem("не важно")
+    combo_box_phone.appendItem("нет")
+    combo_box_phone.appendItem("есть")
 
-    @combo_box_phone.currentItem = 0
+    combo_box_phone.currentItem = 0
 
-    @phone_field = FXTextField.new(frame_left, 20)
-    @phone_field.text = "Поиск по телефону"
+    phone_field = FXTextField.new(@frame_left, 20)
+    phone_field.text = "Поиск по телефону"
 
-    @phone_field.enabled = false
+    phone_field.enabled = false
 
-    @combo_box_phone.connect(SEL_COMMAND) do
-      case @combo_box_phone.currentItem
+    combo_box_phone.connect(SEL_COMMAND) do
+      case combo_box_phone.currentItem
       when 0
-        @phone_field.enabled = false 
+        phone_field.enabled = false 
       when 1
-        @phone_field.enabled = false        # Поле полностью отключено
+        phone_field.enabled = false        # Поле полностью отключено
       when 2
-        @phone_field.enabled = true         # Поле доступно для ввода
-        @phone_field.editable = true
+        phone_field.enabled = true         # Поле доступно для ввода
+        phone_field.editable = true
       end
     end
 
 
     # Telegram
-    FXLabel.new(frame_left, "Наличие Telegram:")
-    @combo_box_telegram = FXComboBox.new(frame_left, 3, opts: COMBOBOX_STATIC | FRAME_SUNKEN | FRAME_THICK)
-    @combo_box_telegram.numVisible = 3
+    FXLabel.new(@frame_left, "Наличие Telegram:")
+    combo_box_telegram = FXComboBox.new(@frame_left, 3, opts: COMBOBOX_STATIC | FRAME_SUNKEN | FRAME_THICK)
+    combo_box_telegram.numVisible = 3
 
-    @combo_box_telegram.appendItem("не важно")
-    @combo_box_telegram.appendItem("нет")
-    @combo_box_telegram.appendItem("есть")
+    combo_box_telegram.appendItem("не важно")
+    combo_box_telegram.appendItem("нет")
+    combo_box_telegram.appendItem("есть")
 
-    @combo_box_telegram.currentItem = 0
+    combo_box_telegram.currentItem = 0
 
-    @telegram_field = FXTextField.new(frame_left, 20)
-    @telegram_field.text = "Поиск по Telegram"
+    telegram_field = FXTextField.new(@frame_left, 20)
+    telegram_field.text = "Поиск по Telegram"
 
-    @telegram_field.enabled = false
+    telegram_field.enabled = false
 
-    @combo_box_telegram.connect(SEL_COMMAND) do
-      case @combo_box_telegram.currentItem
+    combo_box_telegram.connect(SEL_COMMAND) do
+      case combo_box_telegram.currentItem
       when 0, 1
-        @telegram_field.enabled = false
+        telegram_field.enabled = false
       when 2
-        @telegram_field.enabled = true
-        @telegram_field.editable = true
+        telegram_field.enabled = true
+        telegram_field.editable = true
       end
     end
+  end
 
-    button_add = FXButton.new(buttom_frame, "Добавить", opts: BUTTON_NORMAL | LAYOUT_CENTER_X)
-    button_update = FXButton.new(buttom_frame, "Обновить", opts: BUTTON_NORMAL | LAYOUT_CENTER_X)
-    button_change = FXButton.new(buttom_frame, "Изменить", opts: BUTTON_NORMAL | LAYOUT_CENTER_X)
-    button_delete = FXButton.new(buttom_frame, "Удалить", opts: BUTTON_NORMAL | LAYOUT_CENTER_X)
-    button_change.enabled = false
-    button_delete.enabled = false
-    puts 1
-
-    table.connect(SEL_CHANGED) do |sender, sel, data|
-      # Получаем индекс выделенной строки
-      selected_rows = table.getSelectedRows
-
-      # Если выделена хотя бы одна строка (и эта строка выделена полностью)
-      if selected_rows.size == 1
-        button_change.enabled = true  # Включаем кнопку
-        button_delete.enabled = true  # Включаем кнопку
-      else
-        button_change.enabled = false  # Включаем кнопку
-        button_delete.enabled = false  # Включаем кнопку
-      end
-    end
-   
-    
-    
-  
+  def setup_table_frame()
+    table = FXTable.new(@frame_right, opts: LAYOUT_FILL)
     # Устанавливаем количество строк и столбцов
     table.setTableSize(5, 9)  # 5 строк, 9 столбцов (по количеству полей)
   
@@ -199,6 +183,13 @@ class StudentListView < FXMainWindow
         table.setItemText(row, col, value)
       end
     end
+  end
+
+  def setup_crud_frame()
+    button_add = FXButton.new(@buttom_frame, "Добавить", opts: BUTTON_NORMAL | LAYOUT_CENTER_X)
+    button_update = FXButton.new(@buttom_frame, "Обновить", opts: BUTTON_NORMAL | LAYOUT_CENTER_X)
+    button_change = FXButton.new(@buttom_frame, "Изменить", opts: BUTTON_NORMAL | LAYOUT_CENTER_X)
+    button_delete = FXButton.new(@buttom_frame, "Удалить", opts: BUTTON_NORMAL | LAYOUT_CENTER_X)
   end
 end
 
